70dfa2a13f8a18bba61710c45aad4facc833411b
Cameron Eldridge <cameldridge@gmail.com>
2020-11-19 23:06:13 -0500

    Add function for pattern matching lumber values

--- app/util/lumber/index.js
+++ app/util/lumber/index.js
@@ -1,9 +1,55 @@
+import { curry, flip, has, prop, zipWith } from 'ramda';
 import ValueGrammar from './value.pegjs';
 
-// We gotta do a bit of weirdness here to get the Struct class out of the pegjs file
-// and onto window so that we can then export it here and use it normally throughout
-// the code.
-ValueGrammar.parse('hello(world)');
-export const Struct = window.Struct;
+export class Struct {
+    constructor(name, contents = undefined) {
+        this.name = name;
+        this.contents = contents;
+    }
+
+    get isAtom() {
+        return this.contents === undefined;
+    }
+}
+
+export class Variable {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+// These are referenced from the grammar, supplied through window because I don't know how else
+// to inject a value into the compiled module?
+window.Struct = Struct;
+window.Variable = Variable;
+
+export class UnificationError extends Error {}
 
 export const parse = (string) => ValueGrammar.parse(string);
+export const extract = curry((string, value) => unify(parse(string), value));
+export const unify = curry((pattern, value) => {
+  console.log(pattern, value);
+  let binding = {};
+  function unifyInner(pattern, value) {
+    if (pattern instanceof Variable) {
+      binding[pattern.name] = value;
+    } else if (pattern instanceof Struct && value instanceof Struct && pattern.name === value.name) {
+      unifyInner(pattern.contents, value.contents);
+    } else if (Array.isArray(pattern) && Array.isArray(value) && pattern.length === value.length) {
+      zipWith(unifyInner, pattern, value);
+    } else if (typeof pattern === 'object' && typeof value === 'object') {
+      const allkeys = union(Object.keys(pattern), Object.keys(value));
+      if (allkeys.every(flip(has)(pattern)) && allkeys.every(flip(has)(value))) {
+        const lhs = allkeys.map(flip(prop)(pattern));
+        const rhs = allkeys.map(flip(prop)(value));
+        zipWith(unifyInner, lhs, rhs);
+      } else {
+        throw new UnificationError;
+      }
+    } else if (pattern !== value) {
+      throw new UnificationError;
+    }
+  }
+  unifyInner(pattern, value);
+  return binding;
+});
--- app/util/lumber/value.pegjs
+++ app/util/lumber/value.pegjs
@@ -1,49 +1,40 @@
 {
-    class Struct {
-        constructor(name, contents = undefined) {
-            this.name = name;
-            this.contents = contents;
-        }
-
-        get isAtom() {
-            return this.contents === undefined;
-        }
-    }
-
-    window.Struct = Struct;
+    const { Struct, Variable } = window;
 }
 
-start = value
+start = pattern
 
 _ = " "*
 __ = " "+
 
-value
+pattern
     = literal
     / struct
+    / name:$variable { return new Variable(name) }
     / list
     / record
 
 literal
-    = value:$decimal { Number.parseFloat(value) }
-    / value:$integer { Number.parseInt(value, 10) }
+    = pattern:$decimal { Number.parseFloat(pattern) }
+    / pattern:$integer { Number.parseInt(pattern, 10) }
     / string
 
 decimal = integer "." integer
 integer = [0-9]+
-string  = '"' contents:('\\"' / [^'"'])* '"' { contents }
+string  = '"' contents:$('\\"' / [^'"'])* '"' { return contents }
 
 struct
-    = name:$atom _ '(' _ contents:value _ ')' { return new Struct(name, contents) }
+    = name:$atom _ '(' _ contents:pattern _ ')' { return new Struct(name, contents) }
     / name:$atom _ contents:record { return new Struct(name, contents) }
     / name:$atom _ contents:list { return new Struct(name, contents) }
     / name:$atom { return new Struct(name) }
 
 list = '[' _ contents:list_entries? _ ']' { return contents || [] }
-list_entries = head:value (_ "," _ tail:value)* { return [head, ...tail] }
+list_entries = head:pattern (_ "," _ tail:pattern)* { return [head, ...tail] }
 
 record = '{' _ contents:fields? _ '}' { return contents || {} }
 fields = head:field (_ "," _ tail:field)* { return Object.fromEntries([head, ...tail]) }
-field = key:$atom _ ":" _ val:value { return [key, val] }
+field = key:$atom _ ":" _ val:pattern { return [key, val] }
 
 atom = [a-z] [A-Za-z0-9_]*
+variable = [A-Z] [A-Za-z0-9_]*
