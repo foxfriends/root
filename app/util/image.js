9e5ace4fa5a421e401f7b982a097705bed2cd5fa
Mikhail Nekrytov <mihail.nekrytov@gmail.com>
2021-02-09 21:58:20 +0300

    add functions to locate images

--- app/util/image.js
+++ app/util/image.js
@@ -9,6 +9,28 @@ export function getVictoryPointImagePath(faction) {
   return `/image/piece/victory-points/token.${faction}-vp.png`;
 }
 
+export function getMapImagePath(map) {
+  return `/image/map/map-${map}.jpg`;
+}
+
 export function getFactionIconPath(faction) {
   return `/image/piece/icons/token.${faction}-icon.png`;
+}
+
+export function getEyrieLeaderPath(leader) {
+  let path = leader ? 'front.' + leader : 'back';
+
+  return `/image/card/eyrie/card-eyrie_leader-${path}.jpg`;
+}
+
+export function getVagabondCharacterPath(character) {
+  let path = character ? 'front.' + character : 'back';
+
+  return `/image/card/vagabond-character/card-vagabond_character-${path}.jpg`;
+}
+
+export function getFactionBoardPath(faction, back) {
+  let side = back ? 'back' : 'front';
+
+  return `/image/board/board-${faction}-${side}.jpg`;
 }
