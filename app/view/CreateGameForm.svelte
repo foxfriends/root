572f106d56bc06bc1d44fc23f1e529c08a7368fe
Mikhail Nekrytov <mihail.nekrytov@gmail.com>
2021-02-09 20:01:47 +0300

    update create-game-form (from my old fork)

--- app/view/CreateGameForm.svelte
+++ app/view/CreateGameForm.svelte
@@ -1,31 +1,34 @@
 <script>
-import { createEventDispatcher } from 'svelte';
-import Box from './component/Box.svelte';
-import Button from './component/Button.svelte';
-import Text from './component/Text.svelte';
-import Assignment from '../types/Assignment';
-import Deck from '../types/Deck';
-import Faction from '../types/Faction';
-import GameMap from '../types/GameMap';
+  import { createEventDispatcher } from 'svelte';
+  import { getFactionIconPath } from '../util/image';
+  import Box from './component/Box.svelte';
+  import Button from './component/Button.svelte';
+  import Text from './component/Text.svelte';
+  import Assignment from '../types/Assignment';
+  import Deck from '../types/Deck';
+  import Faction from '../types/Faction';
+  import GameMap from '../types/GameMap';
 
-let name = '';
-let factions = [Faction.MARQUISE, Faction.EYRIE, Faction.ALLIANCE, Faction.VAGABOND];
-let assignment = Assignment.CHOOSE;
-let map = GameMap.AUTUMN;
-let deck = Deck.STANDARD;
-$: valid = name && factions.length >= 2;
-$: settings = { factions, assignment, map, deck };
+  let name = '';
+  let factions = [Faction.MARQUISE, Faction.EYRIE, Faction.ALLIANCE, Faction.VAGABOND];
+  let assignment = Assignment.CHOOSE;
+  let map = GameMap.AUTUMN;
+  let deck = Deck.STANDARD;
+  $: valid = name && factions.length >= 2;
+  $: settings = { factions, assignment, map, deck };
 
-const dispatch = createEventDispatcher();
-const back = () => dispatch('back');
-const next = () => name && dispatch('next', { name, settings });
+  const dispatch = createEventDispatcher();
+  const back = () => dispatch('back');
+  const next = () => name && dispatch('next', { name, settings });
 </script>
 
 <Box flex>
   <Button on:click={back}>
     <Text text='back' />
   </Button>
-  <h1 class='heading'><Text text='game-name' /></h1>
+  <h1 class='heading'>
+    <Text text='game-name' />
+  </h1>
   <!-- TODO [l10n]: the placeholder is not localized -->
   <label>
     <input
@@ -33,23 +36,34 @@ const next = () => name && dispatch('next', { name, settings });
       placeholder='Name'
       bind:value={name} />
   </label>
-  <h1 class='heading'><Text text='options' /></h1>
+  <h1 class='heading'>
+    <Text text='options' />
+  </h1>
   <div class='options'>
     <fieldset>
-      <legend><Text text='available-factions' /></legend>
+      <legend>
+        <Text text='available-factions' />
+      </legend>
       {#each Object.values(Faction) as faction}
-        <label>
-          <input
-            type='checkbox'
-            bind:group={factions}
-            value={faction} />
-          <Text text={faction} params={{ form: 'long' }}/>
-        </label>
+        {#if ['cult', 'riverfolk', 'vagabond2', 'duchy', 'conspiracy'].indexOf(faction) === -1}
+          <label class="faction">
+            <input
+              type='checkbox'
+              class="faction__checkbox"
+              bind:group={factions}
+              value={faction}
+            />
+            <img src={getFactionIconPath(faction)} class="faction__icon" />
+            <Text text={faction} params={{ form: 'long' }} />
+          </label>
+        {/if}
       {/each}
     </fieldset>
     <div class='flex'>
       <fieldset>
-        <legend><Text text='faction-assignment' /></legend>
+        <legend>
+          <Text text='faction-assignment' />
+        </legend>
         <label>
           <input disabled type='radio' bind:group={assignment} value='random' />
           <Text text='random' />
@@ -60,19 +74,29 @@ const next = () => name && dispatch('next', { name, settings });
         </label>
       </fieldset>
       <fieldset>
-        <legend><Text text='map' /></legend>
+        <legend>
+          <Text text='map' />
+        </legend>
         {#each Object.values(GameMap) as gameMap}
           <label>
-            <input disabled={gameMap !== GameMap.AUTUMN} type='radio' bind:group={map} value={gameMap} />
+            <input disabled={gameMap !== GameMap.AUTUMN}
+                   type='radio'
+                   bind:group={map}
+                   value={gameMap} />
             <Text text={gameMap} />
           </label>
         {/each}
       </fieldset>
       <fieldset>
-        <legend><Text text='deck' /></legend>
+        <legend>
+          <Text text='deck' />
+        </legend>
         {#each Object.values(Deck) as deckName}
           <label>
-            <input disabled={deckName !== Deck.STANDARD} type='radio' bind:group={deck} value={deckName} />
+            <input disabled={deckName !== Deck.STANDARD}
+                   type='radio'
+                   bind:group={deck}
+                   value={deckName} />
             <Text text={deckName} />
           </label>
         {/each}
@@ -87,29 +111,56 @@ const next = () => name && dispatch('next', { name, settings });
 </Box>
 
 <style>
-.heading {
-  font-family: var(--font-family--display);
-  font-size: 20px;
-  font-weight: 400;
-}
+  .heading {
+    font-family: var(--font-family--display);
+    font-size: 20px;
+    font-weight: 400;
+  }
 
-.input {
-  box-sizing: border-box;
-  padding: 8px;
-  border: none;
-  background-color: transparent;
-  width: 100%;
-  border-bottom: 1px solid var(--color--accent);
-  font-family: var(--font-family--body);
-  font-size: 16px;
-}
+  .input {
+    box-sizing: border-box;
+    padding: 8px;
+    border: none;
+    background-color: transparent;
+    width: 100%;
+    border-bottom: 1px solid var(--color--accent);
+    font-family: var(--font-family--body);
+    font-size: 16px;
+  }
 
-.options {
-  display: flex;
-  align-items: flex-start;
-}
+  .options {
+    display: flex;
+    align-items: flex-start;
+  }
 
-label {
-  display: block;
-}
+  label {
+    display: block;
+  }
+
+  .faction {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    margin-bottom: 5px;
+    cursor: pointer;
+    width: 48%;
+  }
+
+  .faction__checkbox {
+    display: none;
+  }
+
+  .faction__checkbox:checked + .faction__icon {
+    opacity: 1;
+    border-color: black;
+  }
+
+  .faction__icon {
+    opacity: 0.5;
+    width: 40px;
+    height: 40px;
+    margin-right: 10px;
+    border: 2px solid transparent;
+    border-radius: 10px;
+  }
 </style>
