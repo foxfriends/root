64f2a6f4ca070c9c8b454fa4a996caaac7e9f546
Mikhail Nekrytov <mihail.nekrytov@gmail.com>
2021-02-09 20:03:42 +0300

    make score line more convenient

--- app/view/Scores.svelte
+++ app/view/Scores.svelte
@@ -1,14 +1,14 @@
 <script>
-import { groupBy, prop } from 'ramda';
-import context from '../context';
-import GameMaps from '../types/GameMap';
-import VictoryPointMarker from './VictoryPointMarker.svelte';
+  import { groupBy, prop } from 'ramda';
+  import context from '../context';
+  import GameMaps from '../types/GameMap';
+  import VictoryPointMarker from './VictoryPointMarker.svelte';
 
-const { state } = context();
+  const { state } = context();
 
 $: track = do {
   switch ($state.map) {
-    case GameMaps.AUTUMN: ({ x: 192, y: 4229 }); break;
+    case GameMaps.AUTUMN: ({ x: 192, y: 4200 }); break;
   }
 };
 
@@ -16,6 +16,6 @@ $: scores = groupBy(prop('points'), $state.factions);
 </script>
 
 <!-- TODO#41: this will not animate well, as pieces are added and removed if they are on the same score -->
-{#each Object.entries(scores) as [score, factions]}
-  <VictoryPointMarker {factions} x={track.x + 146 * score} y={track.y} />
+{#each Object.entries(scores) as [score, factions], i}
+  <VictoryPointMarker {factions} x={track.x + 148 * score} y={track.y} />
 {/each}
