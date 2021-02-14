7b8c62176b59064f14a855d4c1b9834fc3cd368a
Cameron Eldridge <cameldridge@gmail.com>
2021-02-13 22:49:55 -0500

    Add dev:noreload script

--- rollup.config.js
+++ rollup.config.js
@@ -9,6 +9,7 @@ import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
 const production = !process.env.ROLLUP_WATCH;
+const reload = !production && !process.env.noreload;
 
 export default {
   input: 'app/index.js',
@@ -60,7 +61,7 @@ export default {
     commonjs(),
 
     // reloads page during serve after every code change
-    !production && livereload('dist'),
+    reload && livereload('dist'),
 
     // copying files to dist, because rollup doesn't do it by itself
     copy({
